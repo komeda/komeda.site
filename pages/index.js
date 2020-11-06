ee973910bb7fbdd5e6200c163264ab22e112f8a4
Ryuichi Komeda <komeda@hivelocity.co.jp>
2020-11-06 23:11:45 +0900

    Update permalink

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ export default function Home() {
         <h1 className={styles.title}>Ryuichi Komeda</h1>
         <p>I'm a web engineer living and working in Tokyo.</p>
         <ul className={styles.sns}>
-          <li><a href="/">GitHub</a></li>
-          <li><a href="/">Twitter</a></li>
-          <li><a href="/">Instagram</a></li>
+          <li><a href="https://github.com/komeda">GitHub</a></li>
+          <li><a href="https://twitter.com/rayfields">Twitter</a></li>
+          <li><a href="https://www.instagram.com/rayfields">Instagram</a></li>
         </ul>
       </main>
     </div>
